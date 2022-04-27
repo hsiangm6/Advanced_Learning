c01e1b1e608c536a33c1f81113d28197075f85e7
hsiangm6桌機 <hsiangm6@gmail.com>
2022-04-27 16:33:50 +0800

    homework

--- 中山程設作業python/test.py
+++ 中山程設作業python/test.py
@@ -1,9 +1,1 @@
-def reverseNumber(n):
-    if n == 0:
-        return 0
-    lastDigit = n%10
-    n //= 10
-    return int(str(lastDigit) + str(reverseNumber(n))) if n else lastDigit
-
-for test in (0, 123000, 120):
-    print(test, reverseNumber(test))
+print('welcome to "python"')
