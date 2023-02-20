35dd22dd93dd4c2532f73e241d3fa9c6882577f1
hsiangm6--筆電 <hsiangm6@gmail.com>
2023-02-20 14:02:54 +0800

    Renew

--- 中山程設作業python/lab_module.py
+++
@@ -1,5 +0,0 @@
-def print_multiplication_table(n):
-    for i in range(1,n+1,1):
-        for j in range(1,n,1):
-            print(f'{i*j:3}',end=" ")
-        print(f'{i*n:3}')
--- 中山程設作業python/main.py
+++
@@ -1,3 +0,0 @@
-import lab_module #print multiplication table
-num_of_row_column=int(input("Please enter the number of rows: "))
-lab_module.print_multiplication_table(num_of_row_column)
