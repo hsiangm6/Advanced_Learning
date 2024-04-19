e5b2ff10a24944369a9a0da64bd9e338fc887a4f
hsiangm6--筆電 <hsiangm6@gmail.com>
2024-04-19 18:41:20 +0800

    Add Gemini Test and Claud Test
    Claud's API requires payment.
    Germini is free.
    GPT's API requires payment.

---
+++ LLM/QA_chatbox.py
@@ -0,0 +1,45 @@
+# If you want to run this code, you need to input "streamlit run QA_chatbox.py" in the terminal
+# load sentimental environment variables
+from dotenv import load_dotenv
+
+import os
+# build and share data apps
+import streamlit as st
+import google.generativeai as genai
+
+load_dotenv()
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+
+def get_gemini_response(question):
+    response = chat.send_message(question, stream=True)
+    return response
+
+
+# initialize our streamlit app
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Gemini LLM Application")
+
+# Initialize session state for chat history if it doesn't exist
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+input = st.text_input("Input: ", key="input")
+submit = st.button("Ask the question")
+
+if submit and input:
+    response = get_gemini_response(input)
+    # Add user query and response to session state chat history
+    st.session_state['chat_history'].append(("You", input))
+    st.subheader("The Response is")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot", chunk.text))
+st.subheader("The Chat History is")
+
+for role, text in st.session_state['chat_history']:
+    st.write(f"{role}: {text}")
